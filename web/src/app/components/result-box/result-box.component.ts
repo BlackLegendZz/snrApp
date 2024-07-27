672ca94698f4ebdcb13db668c159bb16bcba3442
Jananas <jan.gellbach@gmail.com>
2024-07-27 18:24:18 +0200

    create plots

--- web/src/app/components/result-box/result-box.component.ts
+++ web/src/app/components/result-box/result-box.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { v4 as uuidv4 } from 'uuid';
 import * as Plotly from 'plotly.js-dist';
 @Component({
   selector: 'app-result-box',
@@ -8,46 +9,31 @@ import * as Plotly from 'plotly.js-dist';
   styleUrl: './result-box.component.css',
 })
 export class ResultBoxComponent implements OnInit {
-  @Input() title = '';
-
+  layout: Partial<Plotly.Layout> = {
+    paper_bgcolor: '#cbd5e1',
+    plot_bgcolor: '#cbd5e1',
+    //height: 350,
+    margin: {
+      l: 20,
+      r: 10,
+      b: 20,
+      t: 10,
+    },
+  };
   ngOnInit(): void {
-    this.drawPlot();
+    this.drawPlot('Result_plot');
+    this.drawPlot('Stacking_plot');
+    this.drawPlot('Skyglow_plot');
   }
 
-  private drawPlot(): void {
-    let trace1: Plotly.Data = {
+  private drawPlot(plotID: string): void {
+    let data: Plotly.Data = {
       x: [1, 2, 3, 4],
       y: [10, 15, 13, 17],
-      mode: 'markers',
-      type: 'scatter',
-    };
-
-    let trace2: Plotly.Data = {
-      x: [2, 3, 4, 5],
-      y: [16, 5, 11, 9],
       mode: 'lines',
       type: 'scatter',
     };
 
-    let trace3: Plotly.Data = {
-      x: [1, 2, 3, 4],
-      y: [12, 9, 15, 12],
-      mode: 'lines+markers',
-      type: 'scatter',
-    };
-
-    let data: Plotly.Data[] = [trace1, trace2, trace3];
-
-    let layout: Partial<Plotly.Layout> = {
-      height: 300,
-      margin: {
-        l: 20,
-        r: 10,
-        b: 20,
-        t: 10,
-      },
-    };
-
-    Plotly.newPlot('plot', data, layout, { responsive: true });
+    Plotly.newPlot(plotID, [data], this.layout, { responsive: true });
   }
 }
