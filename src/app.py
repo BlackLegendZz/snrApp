c5081ba8c5f4bd81c896a4f7b40d585b6dfcc595
Jananas <jan.gellbach@gmail.com>
2024-08-09 18:58:51 +0200

    adding/reworking calculations

--- src/app.py
+++ src/app.py
@@ -1,17 +1,36 @@
 from flask import Flask, render_template, request
+import json
 import calculations
 
 app = Flask(__name__)
 
+default_headers = [
+    ("Access-Control-Allow-Origin", "*"),
+    ("Access-Control-Allow-Headers", "Content-Type,Authorization"),
+    ("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE")
+]
+
+@app.after_request
+def after_request(response):
+    for (name, value) in default_headers:
+        response.headers.add(name, value)
+    return response
 
 @app.route('/')
 def home():
     return render_template("index.html")
 
 
-@app.route("/calculate", methods=["POST"])
+@app.route("/api/calculate", methods=["POST"])
 def caulculateSNR():
-    return request.form 
+    data: dict[str, dict[str, int|float]] = json.loads(request.data)
+    try:
+        snr = calculations.calculateSNR(data)
+        stackingEffect, imgs = calculations.calculateStackingEffect(snr, int(data["Camera"]["Frames"]))
+        skyglowEffect, percs = calculations.calculateSkyglowEffect(data)
+    except Exception as ex:
+        return f"{type(ex).__name__}: {ex}", 400
+    return {"snr": snr, "stacking effect": [stackingEffect, imgs], "skyglow effect": [skyglowEffect, percs]}
 
 
 if __name__ == "__main__":
--- src/calculations.py
+++ src/calculations.py
@@ -1,38 +1,38 @@
 from math import sqrt
-from datatypes import *
 
 
-def calculateSNR(imageStats: ImageStatistics, camera: Camera, newExposure: int | None = None) -> float:
+def calculateSNR(data: dict[str, dict[str, int|float]], newExposure: int | None = None) -> float:
     """Calculates the Signal-to-Noise ratio of a lightframe based on information about the targets and backgrounds
     brightness, as well as camera settings and calibration frames.
 
     Args: imageStats (ImageStatistics): Information about the lightframe and calibration frames (dark and bias)
-    camera (Camera): The used camera newExposure (int | None, optional): Required to re-calculate the SNR based on an
+    camera (Camera): The used camera 
+    newExposure (int | None, optional): Required to re-calculate the SNR based on an
     exposure different from the original one. Defaults to None.
 
     Returns:
         float: The Signal-to-Noise ratio of a light frame
     """
-    if imageStats.SkyglowBackground >= imageStats.TargetSignal:
+    if data["Light Frame"]["Background"] >= data["Light Frame"]["Target"]:
         return -1
 
-    eDarkSignal = (imageStats.DarkSignal - imageStats.BiasSignal) * camera.Gain
-    eSkyglowSignal = (imageStats.SkyglowBackground - imageStats.DarkSignal) * camera.Gain
-    eTargetSignal = (imageStats.TargetSignal - imageStats.SkyglowBackground) * camera.Gain
+    eDarkSignal = (data["Dark Frame"]["Background"] - data["Bias Frame"]["Background"]) * data["Camera"]["Gain"]
+    eSkyglowSignal = (data["Light Frame"]["Background"] - data["Dark Frame"]["Background"]) * data["Camera"]["Gain"]
+    eTargetSignal = (data["Light Frame"]["Target"]- data["Light Frame"]["Background"]) * data["Camera"]["Gain"]
 
     if newExposure:
-        eDarkSignal = newExposure * eDarkSignal / imageStats.ExposureDurationSeconds
-        eSkyglowSignal = newExposure * eSkyglowSignal / imageStats.ExposureDurationSeconds
-        eTargetSignal = newExposure * eTargetSignal / imageStats.ExposureDurationSeconds
+        eDarkSignal = newExposure * eDarkSignal / data["Camera"]["Exposure"]
+        eSkyglowSignal = newExposure * eSkyglowSignal / data["Camera"]["Exposure"]
+        eTargetSignal = newExposure * eTargetSignal / data["Camera"]["Exposure"]
 
     totalSignal = eDarkSignal + eSkyglowSignal + eTargetSignal
-    totalNoise = sqrt(totalSignal + camera.ReadNoise ** 2)
+    totalNoise = sqrt(totalSignal + data["Camera"]["Read Noise"] ** 2)
     targetSNR = eTargetSignal / totalNoise
 
     return round(targetSNR, 6)
 
 
-def calculateStackingEffect(snr: float, frames: int) -> list[float]:
+def calculateStackingEffect(snr: float, frames: int) -> tuple[list[float], list[int]]:
     """Calculates the effect stacking process has on the SNR. Stacking multiple images is known to improve the
     overall SNR of the final, stacked image.
 
@@ -44,18 +44,21 @@ def calculateStackingEffect(snr: float, frames: int) -> list[float]:
         list[float]: The SNRs for every new image that got stacked.
     """
     snrList = [round(sqrt(i + 1) * snr, 2) for i in range(frames)]
-    return snrList
+    return snrList, list(range(frames))
 
 
-def __main():
-    stats = ImageStatistics(2627, 2561, 1999, 1997, 300)
-    cam = Camera(1, 1)
-    percentages = [0.1, 0.25, 0.5, 0.75, 1, 1.5, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    snr = calculateSNR(stats, cam)
-    print(snr)
-    print(calculateStackingEffect(snr, 50))
-    print([calculateSNR(stats, cam, int(stats.ExposureDurationSeconds * perc)) for perc in percentages])
+def calculateSkyglowEffect(data: dict[str, dict[str, int|float]]) -> tuple[list[float], list[int]]:
+    percs = list(range(10, 210, 10))
+    skyglowSNR = []
 
-
-if __name__ == "__main__":
-    __main()
+    eDarkSignal = (data["Dark Frame"]["Background"] - data["Bias Frame"]["Background"]) * data["Camera"]["Gain"]
+    eSkyglowSignal = (data["Light Frame"]["Background"] - data["Dark Frame"]["Background"]) * data["Camera"]["Gain"]
+    eTargetSignal = (data["Light Frame"]["Target"]- data["Light Frame"]["Background"]) * data["Camera"]["Gain"]
+        
+    for change in percs:
+        eNewSkyglowSignal = eSkyglowSignal * change / 100
+        totalSignal = eDarkSignal + eNewSkyglowSignal + eTargetSignal
+        totalNoise = sqrt(totalSignal + data["Camera"]["Read Noise"] ** 2)
+        targetSNR = eTargetSignal / totalNoise
+        skyglowSNR.append(targetSNR)
+    return skyglowSNR, percs
