1e3c2a0421ef45b6138d41c5f203a7444779bb47
Jananas <jan.gellbach@gmail.com>
2024-08-09 19:02:19 +0200

    send response over eventbus

--- web/src/app/components/frame-box/frame-box.component.ts
+++ web/src/app/components/frame-box/frame-box.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InputFieldComponent } from '../input-field/input-field.component';
 import { NetworkService } from '../../services/network.service';
+import { EventService, EventID } from '../../services/event.service';
 
 export enum FrameBox {
   LightFrame = 'Light Frame',
@@ -18,18 +19,21 @@ export enum FrameBox {
   styleUrl: './frame-box.component.css',
 })
 export class FrameBoxComponent {
-  data: { [k: string]: { [k: string]: string } } = {
+  data: { [k: string]: { [k: string]: Number } } = {
     'Light Frame': {},
     'Dark Frame': {},
     'Bias Frame': {},
     Camera: {},
   };
 
-  constructor(private networkService: NetworkService) {}
+  constructor(
+    private networkService: NetworkService,
+    private eventService: EventService
+  ) {}
   sendData() {
     this.networkService.sendData(this.data).subscribe({
       next: (response: any) => {
-        console.log(response);
+        this.eventService.emit(EventID.DrawGraph, response);
       },
       error: (err: any) => {
         console.log(err);
